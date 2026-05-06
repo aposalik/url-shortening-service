dafce77457fd8c45f7146a15757babebf46c204f
Abdullah Salık <abdullahsalik@Abdullahs-MacBook-Air.local>
2026-05-06 15:50:00 +0300

    Clean up: add .gitignore, remove build artifacts and IDE files

--- CreateUrlRequest.java
+++
@@ -1,8 +0,0 @@
-package com.urlshortener.dto;
-
-public class CreateUrlRequest {
-    private String url;
-
-    public String getUrl() { return url; }
-    public void setUrl(String url) { this.url = url; }
-}
